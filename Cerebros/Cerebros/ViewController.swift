a6d1de7ddc5b844b1fc62ac5b770cba74907dccf
DONALD CHWOJKO <dchwojko@DONALDs-MacBook-Pro.local>
2016-11-09 09:52:15 -0600

    initial commit with empty project

---
+++ Cerebros/Cerebros/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cerebros
+//
+//  Created by DONALD CHWOJKO on 11/9/16.
+//  Copyright © 2016 DONALD CHWOJKO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
